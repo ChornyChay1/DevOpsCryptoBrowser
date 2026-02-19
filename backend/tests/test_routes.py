dad8616596cf750df78018d897ab69d29fea854a
Chay <edcroo10@gmail.com>
2026-02-19 23:40:23 +0300

    Рефактор

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,43 +1,21 @@
+from unittest.mock import patch, AsyncMock
+
 import pytest
-from unittest.mock import patch, AsyncMock, MagicMock
 
 
 # =====================================================
 # CREATE
 # =====================================================
 
-def test_create_indicator(client, sample_indicator):
+def test_create_indicator(client, sample_indicator, mock_db_session):
+    # мок recalc_indicator
+    with patch(
+        "api.indicator_routes.recalc_indicator",
+        new_callable=AsyncMock
+    ) as mock_recalc:
+        mock_recalc.return_value = None
 
-    # создаём мок-сессию
-    mock_session = AsyncMock()
-    mock_session.__aenter__.return_value = mock_session
-    mock_session.__aexit__.return_value = None
-
-    mock_session.add = MagicMock()
-    mock_session.commit = AsyncMock()
-    mock_session.refresh = AsyncMock()
-
-    async def refresh_side_effect(ind):
-        ind.id = 1
-
-    mock_session.refresh.side_effect = refresh_side_effect
-
-    # мок get_session_local
-    with patch("api.indicator_routes.get_session_local") as mock_get_session_local:
-
-        # get_session_local() → возвращает фабрику
-        # фабрика() → возвращает mock_session
-        mock_get_session_local.return_value = MagicMock(
-            return_value=mock_session
-        )
-
-        # мок recalc_indicator
-        with patch(
-            "api.indicator_routes.recalc_indicator",
-            new_callable=AsyncMock
-        ) as mock_recalc:
-
-            resp = client.post("/indicator", json=sample_indicator)
+        resp = client.post("/indicator", json=sample_indicator)
 
     assert resp.status_code == 200
     data = resp.json()
