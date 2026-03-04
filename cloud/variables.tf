f5b7695dd971a155b466ac6c60171e39c3711856
Chay <edcroo10@gmail.com>
2026-03-04 17:33:14 +0300

    Ансибл

---
+++ cloud/variables.tf
@@ -0,0 +1,10 @@
+variable "yc_token" {
+  description = "Yandex Cloud OAuth token"
+  type        = string
+  sensitive   = true
+}
+
+variable "folder_id" {
+  description = "Yandex Cloud folder ID"
+  type        = string
+}
